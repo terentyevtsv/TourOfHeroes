92629a34c54f119b42c3eafe6e438b5f013da309
Terentiev_SV <TerentievSV@samnipineft.ru>
2019-10-25 16:40:07 +0400

    Начало роутинга

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule   // Применение модуля форм для двойной привязки
+    FormsModule,
+    AppRoutingModule   // Применение модуля форм для двойной привязки
   ],
   providers: [],
   bootstrap: [AppComponent]
